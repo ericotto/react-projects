81ef3c992532d7c42ddaf347376b21e7cd0dab69
Eric Otto <theottomator@gmail.com>
2016-12-14 12:14:47 -0500

    dev-server fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,23 +2,24 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-    entry: {
-        a: path.resolve(__dirname, "./js/routes.js")
-    },
-    output: {
-        path: path.resolve(__dirname, './js/'),
-        filename: 'bundle.js'
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015,presets[]=stage-2'],
-                exclude: /node_modules/
-            }
-        ]
-    }
+  entry: {
+    a: path.resolve(__dirname, "./js/routes.js")
+  },
+  output: {
+    path: path.resolve(__dirname, './js/'),
+    filename: 'bundle.js',
+    publicPath: '/js/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['react-hot', 'babel?presets[]=react,presets[]=es2015,presets[]=stage-2'],
+        exclude: /node_modules/
+      }
+    ]
+  }
 }
